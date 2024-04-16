3f165b4353f96c922d0c40d03be8b9463253c69a
JHenriquepereira <Jhp.docs@gmail.com>
2024-04-16 19:49:33 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('alterarTema').addEventListener('click', function() {
+    document.documentElement.style.setProperty('--cor-primaria','#282c34');
+    document.documentElement.style.setProperty('--cor-primaria','#c7c2fe');
+});
